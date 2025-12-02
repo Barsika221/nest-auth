e2670794aecfd22bdba4588466c16c419eb97d6b
Barsika221 <kovacs.bars@gmail.com>
2025-12-02 10:41:27 +0100

    feat: add authentication module with login functionality
    - Created AuthController and AuthService for handling authentication logic.
    - Implemented login functionality using argon2 for password hashing.
    - Added User and Token models in Prisma schema for user management.
    - Introduced DTOs for user creation and login validation.
    - Updated UsersService to include user creation and token generation.
    - Added unit tests for AuthController and AuthService.
    - Updated app.module.ts to include UsersModule and AuthModule.
    - Added necessary dependencies in package.json and package-lock.json.

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, ForbiddenException, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { UsersService } from 'src/users/users.service';
+import * as argon2 from 'argon2';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    const user = await this.usersService.findByEmail(loginDto.email);
+    if (user === null) {
+      throw new ForbiddenException('Invalid email or password');
+    }
+    if (!(await argon2.verify(user.password, loginDto.password))) {
+      throw new ForbiddenException('Invalid email or password');
+    }
+    return {
+      token: await this.usersService.createToken(user.id),
+    };
+    // TODO: jelszó stimmel, ha igen, akkor token generálás és visszaadás
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma.service';
+import * as argon2 from 'argon2';
+import * as crypto from 'crypto';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly db: PrismaService) {}
+  async create(createUserDto: CreateUserDto) {
+    const newUser = {
+      ...createUserDto,
+      role: 'USER',
+      password: await argon2.hash(createUserDto.password),
+    };
+    return await this.db.user.create({
+      data: newUser,
+    });
+  }
+
+  async createToken(id: number) {
+    const newToken = crypto.randomBytes(32).toString('hex');
+    await this.db.token.create({
+      data: {
+        token: newToken,
+        user: {
+          connect: { id },
+        },
+      },
+    });
+    return newToken;
+  }
+
+  findByEmail(email: string) {
+    return this.db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
